4fd3e02bce92ae51de77d1f61a63aaa6a40fa799
kikos2359 <kikos2359@gmail.com>
2020-07-04 21:29:06 +0800

    Add GraphUI graph visualization class.

--- src/main/java/org/apd/App.java
+++ src/main/java/org/apd/App.java
@@ -1,6 +1,8 @@
 package org.apd;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -32,11 +34,26 @@ public class App extends Application {
 
         appUI.btnStart.setOnAction(actionEvent -> {
             stage.setScene(appUI.sceneMain);
+            GraphUI.drawGraph(appUI.boxDraw, 8);
             stage.setResizable(true);
         });
 
         appUI.btnEditGraph.setOnAction(actionEvent -> {
             stageEdit.show();
+        });
+
+        appUI.boxDraw.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                GraphUI.moveGraphX(t1.doubleValue() - number.doubleValue());
+            }
+        });
+
+        appUI.boxDraw.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                GraphUI.moveGraphY(t1.doubleValue() - number.doubleValue());
+            }
         });
     }
 
